5d036cf0e89ca7cbe138013c1402f2df943db3e6
Ranaivonampoizina Mikajy <mikamickswonder@gmail.com>
2026-06-23 20:51:34 +0300

    Extract API URL to environment variable

--- frontend/src/redux/slices/authSlice.js
+++ frontend/src/redux/slices/authSlice.js
@@ -1,6 +1,6 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = `${process.env.REACT_APP_API_URL}/auth`;
 
 // Async thunk for user login
 export const loginUser = createAsyncThunk(
